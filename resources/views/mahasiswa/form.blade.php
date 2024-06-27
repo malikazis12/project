4767c3978d69719d718a1a6b6c86825e36c671bb
malikazis12 <147013580+malikazis12@users.noreply.github.com>
2024-06-27 18:49:40 +0700

    penambahan DETAIL & FOTO

--- resources/views/mahasiswa/form.blade.php
+++ resources/views/mahasiswa/form.blade.php
@@ -24,7 +24,7 @@
         </div>
         </div>
         <div class="card-body">
-            <form method="post" action="/mahasiswa/store/">
+            <form method="post" action="/mahasiswa/store/" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-3">
                     <label class="form-label">NIM</label>
@@ -77,6 +77,10 @@
                         <option value="Buddha">Buddha</option>
                         <option value="lainnya">Lainnya</option>
                     </select>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Foto</label>
+                    <input type="file" class="form-control" name="foto" accept="image/*">
                 </div>
                 <button type="submit" class="btn btn-primary">Tambah Data</button>
             </form>
